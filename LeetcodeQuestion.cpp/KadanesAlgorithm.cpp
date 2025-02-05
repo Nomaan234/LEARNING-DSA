14aea21682520e7e72a8c3b46c9c3e891f0d17d2
Nomaan234 <mdnomaan234@gmail.com>
2025-02-05 23:28:49 +0530

    practise

--- LeetcodeQuestion.cpp/KadanesAlgorithm.cpp
+++ LeetcodeQuestion.cpp/KadanesAlgorithm.cpp
@@ -8,7 +8,7 @@ int main ()
     cin>>n;
 
     int arr[n];
-    cout<<"enter the element of the array : ";
+      cout<<"enter the element of the array : ";
     for (int i=0 ; i<n ; i++)
     {
         cin>>arr[i];
@@ -20,22 +20,63 @@ int main ()
     }
     cout<<endl;
 
-    int maxSum = INT_MIN ;
+//    brute force approch...........
 
-    for (int i = 0 ; i < n ; i++)
-    {
-        for(int j = i ; j< n ; j++)
-        {
-            int sum =0;
+//   int maxSum = INT_MIN ;
+
+//     for (int i = 0 ; i < n; i++)
+//     {
+        
+//         for(int j = i ; j< n ; j++)
+//         {
+//             int sum =0;
             
-            for( int k = i ; k<=j ; k++ )
-            {
-                sum += arr[k];
+//             for( int k = i ; k<=j ; k++ )
+//             {
+//                 sum += arr[k];
+//                 maxSum = max(sum , maxSum);
+//             }
+//             cout<<"  ";
+//         }
+//         cout<<endl;
+//     }
+//     cout<< maxSum; 
+
+
+// optimized approch..........
+
+// int maxSum = INT_MIN ;
+
+//     for (int i = 0 ; i < n; i++)
+//     {
+        
+//          int sum =0;
+//         for(int j = i ; j< n ; j++)
+//         {
+//                 sum += arr[j];
+//                 maxSum = max(sum , maxSum);
+            
+//             cout<<"  ";
+//         }
+//         cout<<endl;
+//     }
+//     cout<< maxSum; 
+
+    // kadane's algorithm
+
+    int maxSum = INT_MIN ;
+    int sum =0;
+    for (int i = 0 ; i < n; i++)
+    {
+                sum += arr[i];
                 maxSum = max(sum , maxSum);
-            }
-            cout<<"  ";
-        }
-        cout<<endl;
+            
+                 if(sum<0)
+                 {
+                    sum=0;
+                 }
+        
+                 cout<<endl;
     }
     cout<< maxSum; 
 }
